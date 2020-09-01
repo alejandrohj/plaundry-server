6d2583f15ec9f3ef4bcf1574bee5ee57e465d901
Lanette Lanting <lanettelanting@gmail.com>
2020-09-01 18:32:51 +0200

    another try

--- routes/deliverer.routes.js
+++ routes/deliverer.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 
-const DelivererModel = require('../models/deliverer.model');
+const DelivererModel = require('../models/Deliverer.Model');
 
 router.post('/deliverer/signin', (req, res) => {
   const {email, password} = req.body;
