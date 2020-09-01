5c30ad6d0b4f72add5b7a678ea560974bc398f48
Lanette Lanting <lanettelanting@gmail.com>
2020-09-01 18:20:57 +0200

    second deploy

---
+++ public/precache-manifest.bdc476b8b8b9f6cfd2077cb7c4d971d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231fa2b4b4eb027d76838d8e2f9fc23e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a61efd6cbd6b7668e6e",
+    "url": "/static/css/2.32b0e0b7.chunk.css"
+  },
+  {
+    "revision": "e1b49a2e8135fcd17016",
+    "url": "/static/css/main.05a4779a.chunk.css"
+  },
+  {
+    "revision": "8a61efd6cbd6b7668e6e",
+    "url": "/static/js/2.7a0f98dd.chunk.js"
+  },
+  {
+    "revision": "b07796a77d851ceb8b8882c8966ed41c",
+    "url": "/static/js/2.7a0f98dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b49a2e8135fcd17016",
+    "url": "/static/js/main.dd0b6786.chunk.js"
+  },
+  {
+    "revision": "bea1612623f8adab8c73",
+    "url": "/static/js/runtime-main.25faaf10.js"
+  }
+]);
