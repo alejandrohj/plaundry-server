70f06a08f225069e09118978b4eace6fbf733aa7
Lanette Lanting <lanettelanting@gmail.com>
2020-09-03 08:50:58 +0200

    another try

--- routes/deliverer.routes.js
+++ routes/deliverer.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 
-const DelivererModel = require('../models/Deliverer.model');
+const DelivererModel = require('../models/Deliverer.Model');
 
 
 router.post('/deliverer/create', (req, res) => {
